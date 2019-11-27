9729e96ae38ee39a815127b369c71312ff1296b4
Chui Hin Wah (Dickson) <vicksonzero@users.noreply.github.com>
2019-11-27 23:43:36 +0800

    Add template, add template example

--- script.js
+++ script.js
@@ -5,7 +5,12 @@ const dataAliases = document.querySelector('#data-aliases');
 const data = document.querySelector('#data');
 const dataResidue = document.querySelector('#data-residue');
 const templateInput = document.querySelector('#template-input');
+const checkboxIterateByJS = document.querySelector('#iterate-by-js');
+const btnApplyTemplate = document.querySelector('#apply-template');
 const output = document.querySelector('#output');
+const btnPreviewHTML = document.querySelector('#preview-html');
+const previewBox = document.querySelector('#preview-box');
+
 
 // console.log([
 //     dataInput,
@@ -20,6 +25,9 @@ dataInput.addEventListener('change', () => onDataChange());
 dataFormat.addEventListener('change', () => onDataChange());
 dataAliases.addEventListener('change', () => onDataChange());
 templateInput.addEventListener('change', () => onDataChange());
+
+btnApplyTemplate.addEventListener('click', () => applyTemplate());
+btnPreviewHTML.addEventListener('click', () => previewHTML());
 
 function onDataChange() {
 
@@ -80,47 +88,84 @@ function onDataChange() {
     );
     // console.log(tokensList);
     data.value = JSON.stringify(tokensList, null, 4);
+    window.tokensList = tokensList;
+}
 
+function applyTemplate() {
     // compile the template
     const template = Handlebars.compile(templateInput.value);
     window.template = template;
     // execute the compiled template and print the output to the console
-    output.value = tokensList.map((tokens) => template(tokens)).join('\n\n');
+
+    if (checkboxIterateByJS.checked) {
+        output.value = tokensList.map((tokens) => template(tokens)).join('\n\n');
+    } else {
+        output.value = template(tokensList);
+    }
+}
+
+function previewHTML() {
+    previewBox.innerHTML = output.value;
 }
 
 function initData() {
-    dataInput.value = 'RegExp.prototype.compile()\n' +
+    dataInput.value = 'RegExp.prototype.compile() 23\n' +
         '(Re-)compiles a regular expression during execution of a script.\n' +
-        'RegExp.prototype.exec()\n' +
+        'RegExp.prototype.exec() 34\n' +
         'Executes a search for a match in its string parameter.\n' +
-        'RegExp.prototype.test()\n' +
+        'RegExp.prototype.test() 56\n' +
         'Tests for a match in its string parameter.\n' +
-        'RegExp.prototype[@@match]()\n' +
+        'RegExp.prototype[@@match]() 18\n' +
         'Performs match to given string and returns match result.\n' +
-        'RegExp.prototype[@@matchAll]()\n' +
+        'RegExp.prototype[@@matchAll]() 14\n' +
         'Returns all matches of the regular expression against a string.\n' +
-        'RegExp.prototype[@@replace]()\n' +
+        'RegExp.prototype[@@replace]() 24\n' +
         'Replaces matches in given string with new substring.\n' +
-        'RegExp.prototype[@@search]()\n' +
+        'RegExp.prototype[@@search]() 123\n' +
         'Searches the match in given string and returns the index the pattern found in the string.\n' +
-        'RegExp.prototype[@@split]()\n' +
+        'RegExp.prototype[@@split]() 12.4\n' +
         'Splits given string into an array by separating the string into substring.\n' +
-        'RegExp.prototype.toSource() \n' +
+        'RegExp.prototype.toSource() 123\n' +
         'Returns an object literal representing the specified object; you can use this value to create a new object. Overrides the Object.prototype.toSource() method.\n' +
-        'RegExp.prototype.toString()\n' +
+        'RegExp.prototype.toString() 44\n' +
         'Returns a string representing the specified object. Overrides the Object.prototype.toString() method.';
 
-    dataFormat.value = 'RegExp.prototype.(.+)\n' +
+    dataFormat.value = 'RegExp.prototype.(.+) ([\\d\\.]+)\n' +
         '(.+)';
 
     dataAliases.value = 'name\n' +
+        'size\n' +
         'description';
 
-    templateInput.value = '<tr>\n' +
+    templateInput.value = '<table>\n' +
+        '{{#each this}}\n' +
+        '<tr style="font-size:{{math size "/" 2}}px;">\n' +
+        '    <td>{{@index}}</td>\n' +
         '    <td>{{name}}</td>\n' +
         '    <td>{{description}}</td>\n' +
-        '</tr>';
+        '</tr>\n' +
+        '{{/each}}\n' +
+        '</table>';
 }
 
+Handlebars.registerHelper("math", function (lValue, operator, rValue, options) {
+    if (arguments.length < 4) {
+        // Operator omitted, assuming "+"
+        options = rValue;
+        rValue = operator;
+        operator = "+";
+    }
+
+    lValue = parseFloat(lValue);
+    rValue = parseFloat(rValue);
+
+    return {
+        "+": lValue + rValue,
+        "-": lValue - rValue,
+        "*": lValue * rValue,
+        "/": lValue / rValue,
+        "%": lValue % rValue
+    }[operator];
+});
 
 initData();
